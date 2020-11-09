80b2cc265a988e2a98bee26ee64ce3c4ec4feb54
UrielHer <67615601+UrielHer@users.noreply.github.com>
2020-11-09 13:11:41 -0600

    Add files via upload

---
+++ VueTransition.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+	el: "#contenedor",
+	data:{ 
+		mostrar: true,
+		mensajes: {
+			transicion: 'Esta es una transicion con VUE',
+			animacion: 'Esta es una animacion con VUE', 
+			animacionCustom: 'Esta es una animacion por libreria',
+ 
+		}
+	},
+	methods:{
+
+	},
+	computed: {
+
+	}
+})
